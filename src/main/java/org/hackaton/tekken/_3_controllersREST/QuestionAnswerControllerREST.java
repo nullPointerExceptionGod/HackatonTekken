fa4f87ee2871f73cf8f7297b83f2387e83121405
POGOUVEIA <pogouveia@outlook.com>
2018-12-14 01:19:10 +0000

    restcontrollers

--- src/main/java/org/hackaton/tekken/_3_controllersREST/QuestionAnswerControllerREST.java
+++ src/main/java/org/hackaton/tekken/_3_controllersREST/QuestionAnswerControllerREST.java
@@ -1,4 +1,81 @@
 package org.hackaton.tekken._3_controllersREST;
 
+
+import org.hackaton.tekken._1_persistence.model.Answer;
+import org.hackaton.tekken._1_persistence.model.Question;
+import org.hackaton.tekken._2_services.QuestionsAnswersService;
+import org.hackaton.tekken._4_DTO.AnswerDto;
+import org.hackaton.tekken._5_converters.AnswerDtoToAnswer;
+import org.hackaton.tekken._5_converters.AnswerToAnswerDto;
+import org.hackaton.tekken._5_converters.QuestionDtoToQuestion;
+import org.hackaton.tekken._5_converters.QuestionToQuestionDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/user")
 public class QuestionAnswerControllerREST {
+
+    private QuestionsAnswersService questionsAnswersService;
+
+    private QuestionDtoToQuestion questionDtoToQuestion;
+    private QuestionToQuestionDto questionToQuestionDto;
+    private AnswerToAnswerDto answerToAnswerDto;
+    private AnswerDtoToAnswer answerDtoToAnswer;
+
+    @Autowired
+    public void setQuestionsAnswersService(QuestionsAnswersService questionsAnswersService) {
+        this.questionsAnswersService = questionsAnswersService;
+    }
+
+    @Autowired
+    public void setQuestionDtoToQuestion(QuestionDtoToQuestion questionDtoToQuestion) {
+        this.questionDtoToQuestion = questionDtoToQuestion;
+    }
+
+    @Autowired
+    public void setQuestionToQuestionDto(QuestionToQuestionDto questionToQuestionDto) {
+        this.questionToQuestionDto = questionToQuestionDto;
+    }
+
+    @Autowired
+    public void setAnswerToAnswerDto(AnswerToAnswerDto answerToAnswerDto) {
+        this.answerToAnswerDto = answerToAnswerDto;
+    }
+
+    @Autowired
+    public void setAnswerDtoToAnswer(AnswerDtoToAnswer answerDtoToAnswer) {
+        this.answerDtoToAnswer = answerDtoToAnswer;
+    }
+
+    /**
+     * Retrieves a representation of the given Question answers
+     *
+     * @param cid the question id
+     * @return the response entity
+     */
+    @RequestMapping(method = RequestMethod.GET, path = "/{cid}/question")
+    public ResponseEntity<List<AnswerDto>> listQuestionAnswers(@PathVariable Integer cid) {
+
+        Question question = questionsAnswersService.getQuestion(cid);
+
+        if (question == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        List<AnswerDto> answerDtos = new ArrayList<>();
+        for (Answer answer : question.getAnswers()) {
+            answerDtos.add(answerToAnswerDto.convert(answer));
+        }
+
+        return new ResponseEntity<>(answerDtos, HttpStatus.OK);
+    }
 }
--- src/main/java/org/hackaton/tekken/_3_controllersREST/UserControllerREST.java
+++ src/main/java/org/hackaton/tekken/_3_controllersREST/UserControllerREST.java
@@ -1,11 +1,21 @@
 package org.hackaton.tekken._3_controllersREST;
 
 
+import org.hackaton.tekken._1_persistence.model.User;
 import org.hackaton.tekken._2_services.UserService;
+import org.hackaton.tekken._4_DTO.UserDto;
 import org.hackaton.tekken._5_converters.UserDtoToUser;
 import org.hackaton.tekken._5_converters.UserToUserDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/user")
@@ -15,5 +25,63 @@ public class UserControllerREST {
     private UserDtoToUser userDtoToUser;
     private UserToUserDto userToUserDto;
 
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
+    @Autowired
+    public void setUserDtoToUser(UserDtoToUser userDtoToUser) {
+        this.userDtoToUser = userDtoToUser;
+    }
+
+    @Autowired
+    public void setUserToUserDto(UserToUserDto userToUserDto) {
+        this.userToUserDto = userToUserDto;
+    }
+
+    /**
+     * Adds a user
+     *
+     * @param userDto          the user DTO
+     * @param bindingResult        the binding result object
+     * @param uriComponentsBuilder the uri components builder
+     * @return the response entity
+     */
+    @RequestMapping(method = RequestMethod.POST, path = {"/", ""})
+    public ResponseEntity<User> addUser(@Valid @RequestBody UserDto userDto, BindingResult bindingResult, UriComponentsBuilder uriComponentsBuilder) {
+
+        if (bindingResult.hasErrors() || userDto.getId() != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        User savedUser = userService.save(userDtoToUser.convert(userDto));
+
+        // get help from the framework building the path for the newly created resource
+        UriComponents uriComponents = uriComponentsBuilder.path("/api/user/" + savedUser.getId()).build();
+
+        // set headers with the created path
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(uriComponents.toUri());
+
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    /**
+     * Retrieves a representation of the given user
+     *
+     * @param id the user id
+     * @return the response entity
+     */
+    @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    public ResponseEntity<UserDto> showUser(@PathVariable Integer id) {
+
+        User user = userService.get(id);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(userToUserDto.convert(user), HttpStatus.OK);
+    }
 }
